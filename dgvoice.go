c6750d64d98b6d0a90228bf29fc89633fb16c6df
gido5731 <gido@clickable.systems>
2020-12-15 16:38:37 +0000

    go from start to run

--- dgvoice.go
+++ dgvoice.go
@@ -170,13 +170,13 @@ func PlayAudioFile(v *discordgo.VoiceConnection, filename string, stop <-chan bo
 	ffmpegbuf := bufio.NewReaderSize(ffmpegout, 16384)
 
 	// Starts the ffmpeg command
-	err = run.Start()
-	go run.Wait()
-	if err != nil {
+	go run.Run()
+	//run.Wait()
+/*	if err != nil {
 		OnError("RunStart Error", err)
 		return
 	}
-
+*/
 	// prevent memory leak from residual ffmpeg streams
 	defer run.Process.Kill()
 
